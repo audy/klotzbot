551482e89acf889d91ad90dacf8e976f9569aeb3
Austin Davis-Richardson <harekrishna@gmail.com>
2014-09-15 23:03:14 -0400

    sqash me

--- migrations/2_add_channels.rb
+++ migrations/2_add_channels.rb
@@ -6,10 +6,13 @@ Sequel.migration do
       primary_key :id
       String :channel
     end
-
+    
     alter_table :messages do
       add_foreign_key :channel_id, :channels
     end
+
+    self[:channels].insert([:channel_id],
+      self[:messages].select(:id, :channel_id).exclude(channel_id: nil))
 
   end
 
