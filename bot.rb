47e6fe22a317a57d8eba6fba5f5bf91d0e97598b
Austin Davis-Richardson <harekrishna@gmail.com>
2016-02-23 19:57:24 -0800

    refactored/DRY

--- bot.rb
+++ bot.rb
@@ -1,8 +1,16 @@
 require './environment.rb'
 
-
 @bot =
   Cinch::Bot.new do
+
+    # same as `on` but only works if the user is OWNER
+    def auth_on type, regex, &block
+      on type, regex do |m|
+        if m.user == OWNER
+          block.yield(m)
+        end
+      end
+    end
 
     # memoize channels
     # channel.name -> channel.id
@@ -12,10 +20,10 @@ require './environment.rb'
     Channel.all.map { |c| $channels[c.name] = c.id }
 
     configure do |c|
-      c.server = ENV['SERVER'] || 'irc.freenode.net'
+      c.server = SERVER || 'irc.freenode.net'
       c.channels = Channel.all.map &:name
-      c.nick = ENV['NICK'] || 'klotztest'
-      c.password = ENV['IRC_PASS']
+      c.nick = NICK || 'klotztest'
+      c.password = IRC_PASS
     end
 
     on :message, /.*/ do |m|
@@ -29,35 +37,26 @@ require './environment.rb'
       })
     end
 
-    on :message, /perrier[:]? stats/ do |m|
-      if m.user.nick == ENV['OWNER']
-        m.reply "#{Message.last.id} messages in #{Channel.count} channels"
-      end
+    auth_on :message, /#{NICK}[:]? stats/ do |m|
+      m.reply "#{Message.last.id} messages in #{Channel.count} channels"
     end
 
-    on :message, /perrier[:]? random/ do |m|
-      if m.user.nick == ENV['OWNER']
-        msg = Message.random
-        m.reply "(#{msg.id}) [#{msg.channel.name}] #{msg.nick}: #{msg.message}"
-      end
+    auth_on :message, /#{NICK}[:]? random/ do |m|
+      msg = Message.random
+      m.reply "(#{msg.id}) [#{msg.channel.name}] #{msg.nick}: #{msg.message}"
     end
 
-    on :message, /perrier[:]? channels/ do |m|
-      if m.user.nick == ENV['OWNER']
-        m.reply puts Channel.all.map(&:name).join(', ')
-      end
+    auth_on :message, /#{NICK}[:]? channels/ do |m|
+      m.reply puts Channel.all.map(&:name).join(', ')
     end
 
-    on :message, /perrier[:]? last/ do |m|
-      if m.user.nick == ENV['OWNER']
-        msg = Message.last
-        m.reply "(#{msg.id}) [#{msg.channel.name}] #{msg.nick}: #{msg.message}"
-      end
+    auth_on :message, /#{NICK}[:]? last/ do |m|
+      msg = Message.last
+      m.reply "(#{msg.id}) [#{msg.channel.name}] #{msg.nick}: #{msg.message}"
     end
-
   end
 
-
+# reconnect automatically
 begin
   @bot.start
 rescue Errno::ECONNRESET
