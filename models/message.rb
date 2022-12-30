08f48e90dca300f6acb60223abed7265f6a5f58f
Austin Davis-Richardson <harekrishna@gmail.com>
2022-12-30 07:25:26 +0000

    Start random after network switch

--- models/message.rb
+++ models/message.rb
@@ -1,11 +1,14 @@
 class Message < Sequel::Model
   many_to_one :channel
 
+
   def self.random
+    # start when we switched to libera.chat
+    start_id = 8178097
     @msg = nil
     last = self.last.id
     while @msg.nil?
-      id = rand(1..last)
+      id = rand(start_id..last)
       @msg = self[id]
     end
     return @msg
